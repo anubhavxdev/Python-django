65ce3e0acdac7157e1b65eebbf00b1614cf98acf
anubhavxdev <anubhav.jaiswal23@lpu.in>
2026-04-24 18:14:42 +0530

    Initial commit - Python basics

---
+++ main.py
@@ -0,0 +1,6 @@
+a = "Anubhav Jaiswal"
+b = 12302387
+c = "BTECH IT"
+print(a)
+print(b)
+print(c)
---
+++ practice.py
@@ -0,0 +1,153 @@
+# =========================
+# PYTHON PRACTICE QUESTIONS
+# =========================
+
+# 1. Hello + Variables
+# Write a Python program that:
+# - Prints "Hello, <your_name>"
+# - Stores your name in a variable and uses it
+
+# a = "Anubhav Jaiswal"
+# print("Hello " + a)
+
+# 2. Comments Practice
+# Write a program that:
+# - Uses single-line and multi-line comments
+# - Explains what the program does
+# - Prints "Learning Python is fun"
+
+# a = 1
+# b = 2
+'''
+a = 1
+b = 2
+'''
+# print("Learning Python is Fun")
+
+
+# 3. Variable Swap (Important)
+# Take two variables a = 10, b = 20
+# Swap their values WITHOUT using a third variable
+
+# a = 10
+# b = 20
+# a, b = b, a #for swapping the values without using the third vairables
+# print(a)
+# print(b)
+
+# 4. Typecasting Basics
+# Take:
+# x = "25"
+# y = "5"
+# - Convert both to integers
+# - Print their sum, multiplication, and division
+
+# x = "25"
+# y = "5"
+#
+# x = int(x)
+# y = int(y)
+#
+# print("sum",x+y)
+# print("Multiply",x*y)
+# print("Divide",x/y)
+
+# 5. User Input + Typecasting
+# Take input from user:
+# - Name (string)
+# - Age (int)
+# Print:
+# Hello Anubhav, you will be 100 years old in XXXX
+#
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# print("hello " + name + " you are " + str(age) + " years old")
+
+# 6. Operators Practice (All Types)
+# Write a program that:
+# - Takes two numbers
+# - Prints:
+#   Addition
+#   Subtraction
+#   Multiplication
+#   Division
+#   Modulus
+#   Power
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# add = a + b
+# sub = a - b
+# mul = a * b
+# div = a / b
+# mod = a % b
+# poem = a ** b
+# print(add)
+# print(sub)
+# print(mul)
+# print(div)
+# print(mod)
+# print(poem)
+
+# 7. Even or Odd (Operator Logic)
+# Take a number from user and:
+# - Print whether it is even or odd using % operator
+#
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+#
+# if(a % 2 == 0):
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+
+# 8. Type Check + Conversion
+# Take a variable:
+# value = 10.5
+# - Print its type
+# - Convert it to int
+# - Convert it to str
+
+# a = float(input("Enter a number: "))
+# print(type(a))
+# a = int(a)
+# print(type(a))
+# a = str(a)
+# print(type(a))
+#
+
+# 9. Simple Calculator (Mini Project)
+# Make a program:
+# - Takes 2 numbers
+# - Takes an operator (+, -, *, /)
+# - Performs operation using if conditions
+#
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# op = input("Enter sign")
+#
+# if op == "+":
+#     print(a + b)
+# elif op == "-":
+#     print(a - b)
+# elif op == "*":
+#     print(a * b)
+# elif op == "/":
+#     print(a / b)
+# else:
+#     print("Invalid operation")
+
+# 10. String + Number Mix (Tricky)
+# Write a program:
+# - Ask user for a number
+# - Print:
+#   You entered: X
+#   Square is: X^2
+#   Cube is: X^3
+
+# a = int(input("Enter a number: "))
+# print("You Entered: ",a)
+# print("Square is: ",a**2)
+# print("Cube is",a**3)
+# =========================
+# END OF QUESTIONS
+# =========================
